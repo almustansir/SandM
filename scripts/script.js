2f44f3c63dbdfe2690b86023eb8302207f906f24
Samit_08 <almustansir465@gmail.com>
2023-01-04 18:29:01 +0600

    navbar made responsive

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar-nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navbar.classList.remove("active");
+  })
+);
